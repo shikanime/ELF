57aec860a22ebbaf5037e11a96402c6ea133b99f
Shikanime Deva <deva.shikanime@protonmail.com>
2019-02-23 21:20:52 +0100

    chore: rename supervisor

--- apps/elven_gard/lib/elven_gard/cluster.ex
+++ apps/elven_gard/lib/elven_gard/cluster.ex
@@ -1,17 +1,12 @@
 defmodule ElvenGard.Cluster do
   use Supervisor
 
-  def start_link(args) do
-    Supervisor.start_link(__MODULE__, args, name: __MODULE__)
-  end
-
-  @impl true
-  def init(_args) do
+  def start_link(_args) do
     children = [
-      {Cluster.Supervisor, [cluster_config(), [name: ElvenGard.ClusterSupervisor]]},
+      {Cluster.Supervisor, [cluster_config(), [name: __MODULE__]]},
     ]
 
-    Supervisor.init(children, strategy: :one_for_one)
+    Supervisor.start_link(children, strategy: :one_for_one)
   end
 
   def cluster_config do
