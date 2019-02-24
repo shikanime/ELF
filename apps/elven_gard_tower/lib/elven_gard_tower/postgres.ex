9d8608a3a5b7309ce4abd63017440a6327216173
Shikanime Deva <deva.shikanime@protonmail.com>
2019-02-24 03:14:00 +0100

    chore: configure ecto

--- apps/elven_gard_tower/lib/elven_gard_tower/postgres.ex
+++ apps/elven_gard_tower/lib/elven_gard_tower/postgres.ex
@@ -2,4 +2,11 @@ defmodule ElvenGardTower.Postgres do
   use Ecto.Repo,
     otp_app: :elven_gard_tower,
     adapter: Ecto.Adapters.Postgres
+ BBelven_gard_tower
+  def init(_type, config) do
+    {:ok, [
+      password: System.get_env("POSTGRES_PWD"),
+      hostname: System.get_env("POSTGRES_HOST")
+    ] ++ config}
+  end
 end
