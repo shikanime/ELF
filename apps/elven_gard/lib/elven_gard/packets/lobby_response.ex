dbed3591eae3f2810768db747a3a741d47f05130
Shikanime Deva <deva.shikanime@protonmail.com>
2019-02-24 13:17:56 -0800

    fix: character list

--- apps/elven_gard/lib/elven_gard/packets/lobby_response.ex
+++ apps/elven_gard/lib/elven_gard/packets/lobby_response.ex
@@ -2,9 +2,15 @@ defmodule ElvenGard.LobbyResponse do
   def render("list_characters.nsl", %{characters: characters}) do
     [
       "clist_start 0",
-      Enum.map(characters, &(render_character(&1))),
+      render_characters(characters),
       "clist_end"
     ]
+  end
+
+  defp render_characters(characters) do
+    characters
+    |> Enum.map(&(render_character(&1)))
+    |> Enum.join(" ")
   end
 
   def render_character(character) do
@@ -14,7 +20,7 @@ defmodule ElvenGard.LobbyResponse do
     #{character.hair_color} 0 #{character.class} \
     #{character.level} #{character.hero_level} \
     #{character.equipments} #{character.job_level} \
-    1 1 #{character.pets} 0
+    1 1 #{character.pets} 0\
     """
   end
 end
