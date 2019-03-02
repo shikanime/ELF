c4eba830c3d53907bd0fe3609034544c9f1770b6
Shikanime Deva <deva.shikanime@protonmail.com>
2019-03-02 19:24:17 +0100

    chore: update apps version

--- apps/elven_gard_universe/mix.exs
+++ apps/elven_gard_universe/mix.exs
@@ -4,7 +4,7 @@ defmodule ElvenGardUniverse.MixProject do
   def project do
     [
       app: :elven_gard_universe,
-      version: "0.1.0",
+      version: "2.0.0-beta.2",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
@@ -24,13 +24,13 @@ defmodule ElvenGardUniverse.MixProject do
 
   defp deps do
     [
-      {:ecto_sql, "~> 3.0"},
-      {:postgrex, ">= 0.0.0"},
       {:espec, "~> 1.6.3", only: :test},
       {:mock, "~> 0.3.0", only: :test},
-      {:uuid, "~> 1.1"},
+      {:libcluster, "~> 3.0"},
       {:swarm, "~> 3.0"},
-      {:libcluster, "~> 3.0"}
+      {:ecto_sql, "~> 3.0"},
+      {:postgrex, ">= 0.0.0"},
+      {:uuid, "~> 1.1"}
     ]
   end
 end
